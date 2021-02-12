bbb9b850862d424e7774197a9ba5aab521f3f241
Fidelis Osikhena <oshomahosikhena@gmail.com>
2021-02-11 20:58:52 -0500

    Added Enemies

---
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform player;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        navMeshAgent.SetDestination(player.position);
+    }
+    
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.collider.tag == "Player")
+        {
+            animator.SetBool("z_attack", true);
+        }
+    }
+
+    }
